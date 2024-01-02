dfb78587afcea6fd305dc435fe8a101030bd05ac
thirumal <thirumalgurram123@gamail.com>
2024-01-02 13:44:22 +0530

    first commit

---
+++ test.js
@@ -0,0 +1,12 @@
+// console.log("thirumal");
+let a =[1,4,6,0,8,5,0,0,0];
+let b =[];
+// console.log(typeof a.length);
+
+for(let i = 0; i<a.length; i++ ){
+   if(a[i]>0)
+   {
+        b.push(a[i])
+    }
+}
+console.log(b);
